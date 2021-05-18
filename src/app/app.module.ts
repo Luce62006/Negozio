5e36e6edb312587a366b659ce24c3bdb13dacaa4
SvetlanaFoglia <luce62006@gmail.com>
2021-05-18 18:01:08 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserCardComponent } from './header1/user-card/user-card.component';
+import { Header1Component } from './header1/header1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCardComponent,
+    Header1Component
   ],
   imports: [
     BrowserModule
